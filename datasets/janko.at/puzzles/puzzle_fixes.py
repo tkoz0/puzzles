64f7378b21a38f977bf128eed46a364222f7e8a5
TKoz Zero <tkozlowski1999@gmail.com>
2020-04-26 16:32:07 -0400

    added fixes for some more puzzles

--- datasets/janko.at/puzzles/puzzle_fixes.py
+++ datasets/janko.at/puzzles/puzzle_fixes.py
@@ -16,6 +16,7 @@ def writeLines(file,lines):
     outf = open(file,'w')
     for line in lines: outf.write(line)
     outf.close()
+    print('fixed',file)
 
 def fixHeyawake(path):
     file1 = path+'/576.a.txt'
@@ -24,9 +25,49 @@ def fixHeyawake(path):
         assert lines[-1] == 'send\n'
         lines[-1] = 'end\n' # fix from 'send' to 'end'
         writeLines(file1,lines)
-        print('fixed',file1)
 
-funcs = {'Heyawake':fixHeyawake}
+def fixFillomino(path):
+    file1 = path+'/020.a.txt'
+    if not os.path.isfile(file1+'.old'):
+        lines = getLines(file1)
+        i = 0
+        for j in range(len(lines)): # remove a single line causing issues
+            if lines[j].startswith('m0'):
+                i = j
+                break
+        lines.pop(i)
+        writeLines(file1,lines)
+
+def fixLITS(path):
+    file1 = path+'/068.a.txt'
+    if not os.path.isfile(file1+'.old'):
+        lines = getLines(file1)
+        lines.insert(0,'begin\n') # missing begin line
+        writeLines(file1,lines)
+    # files with the "areas" line repeated
+    doublearea = [path+('/%d.a.txt'%p) for p in
+        [281,282,284,285,286,287,288,289,290] ]
+    for file in doublearea:
+        if not os.path.isfile(file+'.old'):
+            lines = getLines(file)
+            assert lines[8] == 'areas\n' and lines[9] == 'areas\n'
+            lines.pop(8)
+            writeLines(file,lines)
+
+def fixNurikabe(path):
+    noend = [path+('/%d.a.txt'%p) for p in
+        [232] ]
+    for file in noend:
+        if not os.path.isfile(file+'.old'):
+            lines = getLines(file)
+            assert lines[-1] != 'end\n'
+            lines.append('end\n')
+            writeLines(file,lines)
+
+funcs = {'Heyawake':fixHeyawake,
+         'Fillomino':fixFillomino,
+         'LITS':fixLITS,
+         'Nurikabe':fixNurikabe}
 
 if __name__ == '__main__':
     indir = os.path.normpath(sys.argv[1])
