5f48123f3ff81d6f177a50f6220b91726efe1e96
TKoz Zero <tkozlowski1999@gmail.com>
2020-04-27 04:50:19 -0400

    alphabetized scrypt order

--- datasets/janko.at/puzzles/make_basic_json.sh
+++ datasets/janko.at/puzzles/make_basic_json.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 # fix errors in txt files (straight from the <script id="data" ..> tags)
-python3 puzzle_fixes.py Heyawake
 python3 puzzle_fixes.py Fillomino
+python3 puzzle_fixes.py Heyawake
 python3 puzzle_fixes.py LITS
 python3 puzzle_fixes.py Nurikabe
 
 # convert the txt files into bulk basic json files
-python3 puzzle_json.py Sudoku
-python3 puzzle_json.py Heyawake
 python3 puzzle_json.py Akari
 python3 puzzle_json.py Fillomino
+python3 puzzle_json.py Heyawake
 python3 puzzle_json.py LITS
 python3 puzzle_json.py Nurikabe
 python3 puzzle_json.py Slitherlink
+python3 puzzle_json.py Sudoku
 python3 puzzle_json.py Sudoku/2D
 python3 puzzle_json.py Sudoku/Butterfly
 python3 puzzle_json.py Sudoku/Chaos
