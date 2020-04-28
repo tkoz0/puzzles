29087c32dd32d2296a63229d8aacf1f2b1958db3
TKoz Zero <tkozlowski1999@gmail.com>
2020-04-28 03:01:20 -0400

    added lines to run parsing on new puzzles

--- datasets/janko.at/puzzles/make_basic_json.sh
+++ datasets/janko.at/puzzles/make_basic_json.sh
@@ -5,6 +5,10 @@ python3 puzzle_fixes.py Fillomino
 python3 puzzle_fixes.py Heyawake
 python3 puzzle_fixes.py LITS
 python3 puzzle_fixes.py Nurikabe
+python3 puzzle_fixes.py Slitherlink
+python3 puzzle_fixes.py Zahlenlabyrinth
+python3 puzzle_fixes.py Zehnergitter
+python3 puzzle_fixes.py Zeltlager-2
 
 # convert the txt files into bulk basic json files
 python3 puzzle_json.py Abc-End-View
@@ -29,3 +33,14 @@ python3 puzzle_json.py Sudoku/Shogun
 python3 puzzle_json.py Sudoku/Sohei
 python3 puzzle_json.py Sudoku/Sumo
 python3 puzzle_json.py Sudoku/Windmill
+python3 puzzle_json.py Zahlenkreuz
+python3 puzzle_json.py Zahlenlabyrinth
+python3 puzzle_json.py Zahlenschlange
+python3 puzzle_json.py Zehnergitter
+python3 puzzle_json.py Zeltlager
+python3 puzzle_json.py Zeltlager-2
+python3 puzzle_json.py Ziegelmauer
+python3 puzzle_json.py Zipline
+
+# puzzles to ignore, such as language based puzzles
+touch puzzle_data/Zitatemix/output.ignore
