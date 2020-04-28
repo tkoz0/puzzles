e911a47bdb118dbb82186c2eb6445b4de2064c09
TKoz Zero <tkozlowski1999@gmail.com>
2020-04-28 02:59:49 -0400

    added support for puzzles to ignore

--- datasets/janko.at/puzzles/puzzle_status.py
+++ datasets/janko.at/puzzles/puzzle_status.py
@@ -17,8 +17,11 @@ def explore(path):
     if not os.path.isdir(path): return
     files = os.listdir(path)
     if hasTxt(files): # has puzzle data, show status
-        done = os.path.isfile(path+'/output.success')
-        results.append(('done' if done else 'notdone',path))
+        if os.path.isfile(path+'/output.ignore'):
+            results.append(('ignore',path))
+        else:
+            done = os.path.isfile(path+'/output.success')
+            results.append(('done' if done else 'notdone',path))
     else: results.append(('notxt',path))
     for f in sorted(files):
         explore(path+'/'+f)
@@ -28,12 +31,16 @@ explore('puzzle_data')
 _,done = zip(*list(filter(lambda x : x[0] == 'done', results)))
 _,notdone = zip(*list(filter(lambda x : x[0] == 'notdone', results)))
 _,notxt = zip(*list(filter(lambda x : x[0] == 'notxt', results)))
+_,ignore = zip(*list(filter(lambda x : x[0] == 'ignore', results)))
 
 print('-- DONE --')
 for dir in done: print(dir)
 print()
 print('-- NO TXT --')
 for dir in notxt: print(dir)
+print()
+print('-- IGNORED --')
+for dir in ignore: print(dir)
 print()
 print('-- NOT DONE --')
 for dir in notdone: print(dir)
